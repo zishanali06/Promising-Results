823da49c9859911f5abe9b7518c7a1a397e2f978
Zishan Ali <zishanali06@gmail.com>
2019-03-14 21:01:11 -0500

    done for sure now

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ const doMath = async () => {
     try {  //MUST USE TRY / CATCH WHEN USING async AND await IN ES6
         let number = await slowMath.add(2, 6);
         console.log(number);
+        //next await below wont work until first await sends back a resolve or reject from that PROMISE
         let number2 = await slowMath.multiply(number, 2); //can use new variables and will still wait for previus await, number variable can be a number instead of variable also
         console.log(number2);
         number = await slowMath.divide(number2, 4);
