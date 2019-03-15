f10928f05295896c7962e0010e6f4f6247ca24cf
Zishan Ali <zishanali06@gmail.com>
2019-03-14 20:59:32 -0500

    both working

--- app.js
+++ app.js
@@ -1,37 +1,41 @@
-// slowMath.add(1, 1)
-// .then((answer) => {
-//     console.log(answer);
-//     return slowMath.multiply(answer, 2);
-// }).then((answer2) => {   //passing in the result from the promise above down to this .then
-//     console.log(answer2);
-//     return slowMath.divide(answer2, 4);
-// }).then((answer) => {
-//     console.log(answer);
-//     return slowMath.subtract(answer, 3);
-// }).then((answer) => {
-//     console.log(answer);
-//     return slowMath.add(answer, 98);
-// }).then((answer) => {
-//     console.log(answer);
-//     return slowMath.remainder(answer, 2);
-// }).then((answer) => {
-//     console.log(answer);
-//     return slowMath.multiply(answer, 50);
-// }).then((answer) =>  {
-//     console.log(answer);
-//     return slowMath.remainder(answer, 40);
-// }).then((answer) => {
-//     console.log(answer);
-//     return slowMath.add(answer, 32);
-// }).then((answer) => {
-//     console.log(`The final result is ${answer}.`);
-// }).catch((error) => {
-//     console.log(error);
-// });
 
+// //THIS IS THE ES5 WAY TO DO IT
+slowMath.add(2, 6)
+.then((answer) => {
+    console.log(answer);
+    return slowMath.multiply(answer, 2);
+}).then((answer2) => {   //passing in the result from the promise above down to this .then
+    console.log(answer2);
+    return slowMath.divide(answer2, 4);
+}).then((answer) => {
+    console.log(answer);
+    return slowMath.subtract(answer, 3);
+}).then((answer) => {
+    console.log(answer);
+    return slowMath.add(answer, 98);
+}).then((answer) => {
+    console.log(answer);
+    return slowMath.remainder(answer, 2);
+}).then((answer) => {
+    console.log(answer);
+    return slowMath.multiply(answer, 50);
+}).then((answer) =>  {
+    console.log(answer);
+    return slowMath.remainder(answer, 40);
+}).then((answer) => {
+    console.log(answer);
+    return slowMath.add(answer, 32);
+}).then((answer) => {
+    console.log(`The final result is ${answer}.`);
+}).catch((error) => {   //MUST HAVE A CATCH TO CATCH ERROR
+    console.log(error);
+});
+
+
+//THIS IS THE ES6 WAY TO DO IT
 const doMath = async () => {
-    try {
-        let number = await slowMath.add(6, 2);
+    try {  //MUST USE TRY / CATCH WHEN USING async AND await IN ES6
+        let number = await slowMath.add(2, 6);
         console.log(number);
         let number2 = await slowMath.multiply(number, 2); //can use new variables and will still wait for previus await, number variable can be a number instead of variable also
         console.log(number2);
@@ -49,7 +53,7 @@ const doMath = async () => {
         console.log(number);
         number = await slowMath.add(number, 32);
         console.log(`The final result is ${number}.`);
-    } catch (error) {
+    } catch (error) {   //THIS IS THE CATCH PART OF THE TRY/CATCH THAT MUST BE IN ES6 WHEN DOING PROMISE CHAIN OR EVEN ONE PROMISE
         console.log(error);
     }
 }
